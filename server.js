78c2bfa7384e9947df2522e6cc67f230b2e9f1be
[Marcelo de Oliveira] <[marcelo701500@gmail.com]>
2023-08-02 16:10:00 -0300

    Sevidor https

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // JSON Server module
+/*
 const jsonServer = require('json-server');
 
 const server = jsonServer.create();
@@ -12,4 +13,24 @@ server.listen(port, '10.0.0.4');
 
 // Export the Server API
 module.exports =server;
+*/
+
+var fs = require('fs'),
+  https = require('https'),
+  jsonServer = require('json-server'),
+  server = jsonServer.create(),
+  router = jsonServer.router('db/db.json'),
+  middlewares = jsonServer.defaults();
+
+var options = {
+  key: fs.readFileSync('./ssl/key.pem'),
+  cert: fs.readFileSync('./ssl/cert.pem')
+};
+
+server.use(middlewares);
+server.use(router);
+
+https.createServer(options, server).listen(3000, function() {
+  console.log("json-server started on port " + 3000);
+});
 
