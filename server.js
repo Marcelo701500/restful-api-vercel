42d4a00b6d4e6754f32f44d7ace0953fcadba985
[Marcelo de Oliveira] <[marcelo701500@gmail.com]>
2023-08-02 15:40:51 -0300

    Atualização de dados

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
-
 // JSON Server module
-const jsonServer = require("json-server");
+const jsonServer = require('json-server');
+
 const server = jsonServer.create();
-const router = jsonServer.router("db/db.json");
+const router = jsonServer.router('db/db.json');
 const middlewares = jsonServer.defaults();
+const port = process.env.port || 3000;
 
 server.use(middlewares);
-server.use(
-	// Add custom route here if needed
-	jsonServer.rewriter({
-		"/api/*": "/$1",
-	})
-);
 server.use(router);
-server.listen(3000, () => {
-	console.log("JSON Server is running");
-});
+server.listen(port, '10.0.0.4');
 
 // Export the Server API
-module.exports = server;
+module.exports =server;
+
